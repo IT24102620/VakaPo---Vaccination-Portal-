afa5293ef69a8c0c62bdfabcc5cbb3170896203e
IT24101071 <it24101071@my.sliit.lk>
2025-09-19 11:03:00 +0530

    Project Structure

---
+++ src/main/java/lk/vakapo/vakapo/VakapoApplication.java
@@ -0,0 +1,13 @@
+package lk.vakapo.vakapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VakapoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VakapoApplication.class, args);
+	}
+
+}
